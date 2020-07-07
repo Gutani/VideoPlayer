8826b99c877d5700df24556443b30ba8a30f8fb3
Gustavo <gustavoamalheiros@gmail.com>
2020-07-06 23:54:24 -0300

    Video Player in Android

---
+++ app/src/main/java/com/magie/videoplayer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.magie.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private ViewHolder mViewHolder = new ViewHolder();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mViewHolder.buttonPlay = findViewById(R.id.buttonPlay);
+        mViewHolder.buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), PlayerActivity.class));
+            }
+        });
+    }
+
+    public static class ViewHolder{
+        Button buttonPlay;
+    }
+}
